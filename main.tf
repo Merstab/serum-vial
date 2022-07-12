1d69b067679b74697e13aa377b05dd18d15d01a8
pan0ply <104095345+pan0ply@users.noreply.github.com>
2022-07-11 22:49:43 -0400

    feat: add iac

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.2"
+  required_providers {
+    aws = {
+      version = "~>4.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {}
+
+module "serum_vial_ecs_service" {
+  source = "./iac"
+
+  desired_count = 1
+  image_version = "1.7.0"
+  cluster_name  = "nebula"
+  task_name     = "serum_vial"
+}
